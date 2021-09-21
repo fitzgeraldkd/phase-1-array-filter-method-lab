350173527b4fc6341cad1d24f20433eeaee3c59b
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-21 17:42:52 -0400

    commit tree b8d17147e8df28f747a1c9d472d066bb794adc8e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverNames, nameToMatch) {
+    return driverNames.filter(name => name.toLowerCase() === nameToMatch.toLowerCase());
+}
+
+function fuzzyMatch(driverNames, nameStart) {
+    return driverNames.filter(name => name.startsWith(nameStart));
+}
+
+function matchName(drivers, nameToMatch) {
+    return drivers.filter(driver => driver.name === nameToMatch);
+}
